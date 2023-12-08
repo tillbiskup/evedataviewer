a73d47ed832a054dc330246cfa262bdc52d9466a
Till Biskup <till@till-biskup.de>
2023-12-08 13:39:10 +0100

    Start writing tests for datasets

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,3 +1,18 @@
 import unittest
 
-from eveviewer import dataset
+from eveviewer import dataset as eve_dataset
+from eveviewer import io as eve_io
+
+
+class TestDataset(unittest.TestCase):
+    def setUp(self):
+        self.dataset = eve_dataset.Dataset()
+
+    def test_instantiate_class(self):
+        pass
+
+    def test_import_from_loads_data(self):
+        importer_factory = eve_io.ImporterFactory()
+        importer = importer_factory.get_importer()
+        self.dataset.import_from(importer)
+        self.assertTrue(self.dataset.data.data.any())
